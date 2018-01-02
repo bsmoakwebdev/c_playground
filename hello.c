21e0461ad4e563c3e64b3a44e0e512fb64543bf3
Bradley Smoak <bsmoakwebdev@gmail.com>
2018-01-02 10:22:18 -0500

    added hello.co and .h

---
+++ hello.c
@@ -0,0 +1,13 @@
+//
+//  hello.c
+//  
+//
+//  Created by Bradley Smoak on 1/2/18.
+//
+
+#include "hello.h"
+
+main()
+{
+    printf("Hello, world\n");
+}
---
+++ hello.h
@@ -0,0 +1,13 @@
+//
+//  hello.h
+//  
+//
+//  Created by Bradley Smoak on 1/2/18.
+//
+
+#ifndef hello_h
+#define hello_h
+
+#include <stdio.h>
+
+#endif /* hello_h */
