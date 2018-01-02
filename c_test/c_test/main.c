c4896b612b5400067fa8c1799fd0adcdfc379d73
Bradley Smoak <bsmoakwebdev@gmail.com>
2018-01-02 10:57:18 -0500

    added c_test dir

---
+++ c_test/c_test/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c_test
+//
+//  Created by Bradley Smoak on 1/2/18.
+//  Copyright © 2018 Bradley Smoak. All rights reserved.
+//
+
+#include <stdio.h>
+
+main()
+{
+    printf("Hello, world!\n");
+}
